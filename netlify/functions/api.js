bfd870f0075ab1ca9467c51613304fc2dcfa6ea0
Atharv <ahtelang16@gmail.com>
2026-01-12 22:00:29 +0530

    Fix Netlify deployment: remove public publish dir, fix views path

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const serverless = require('serverless-http');
+const path = require('path');
 
 const app = express();
 
@@ -22,9 +23,12 @@ let books = [
     }
 ];
 
+// Set views directory
+app.set('views', path.join(__dirname, '../../view'));
+
 // Middleware
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../../public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
